253e1eda173f0d24d4a529dd00ed8a93348a8f51
Wilson Montelo <wilson.montelo@hotmail.com>
2024-03-26 12:02:09 -0300

    feat: create config init nodejs

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors  from 'cors';
+import mongoose from 'mongoose';
+
+export class App {
+  private express: express.Application
+  private porta = 9000
+
+  constructor() {
+    this.express = express()
+    this.listen()
+  }
+
+  public getApp(): express.Application {
+    return this.express
+  }
+
+  private listen(): void {
+    this.express.listen(this.porta, () => {
+      console.log('Servidor iniciado na porta ' + this.porta)
+    })
+  }
+}
